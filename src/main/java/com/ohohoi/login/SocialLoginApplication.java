c939583a9974b4d50305806aa738bcc274f5141a
ohohoi <ohohoi@sookmyung.ac.kr>
2020-10-11 23:30:15 +0900

    Cache Clear

--- src/main/java/com/ohohoi/login/SocialLoginApplication.java
+++ src/main/java/com/ohohoi/login/SocialLoginApplication.java
@@ -2,12 +2,30 @@ package com.ohohoi.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @SpringBootApplication
-public class SocialLoginApplication {
+public class SocialLoginApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(SocialLoginApplication.class, args);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+        // @formatter:on
+    }
 }
